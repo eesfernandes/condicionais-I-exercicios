5c61f08b030447f5b3ae06947a30f5472dbfb85d
eesfernandes <eesf@hotmail.com>
2023-02-23 15:22:01 -0300

    easley-eric-fernandes-a012

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,11 @@
-const num = //seu numero aqui
-
-if(){
-    console.log("sua mensagem aqui");
+function ePar (){
+    const num = parseInt(prompt('Digite um número: '))
+    if(num % 2 === 0){
+        const msgPar = `O número "${num}" digitado é Par`
+        return msgPar
+    }else{
+        const outraMsg = `O número "${num}" digitado não é Par`
+        return outraMsg
+    }
 }
+console.log(ePar())
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,30 @@
-let idade;
-let isTerminouEnsinoMedio;
-let isCursandoOutraFaculdade;
-
-if(){
-
-}else{
-	
+function tresValores (){
+    const idade = parseInt(prompt('Digite sua idade: '))
+    let perEnsinoMedio = prompt('Ensino médio concluído? (Responda sim ou não): ')
+    let perFaculdade = prompt('Está cursando alguma faculdade? (Responda sim ou não): ')
+    if(idade >= 18){
+        const msgMaiorIdade = `Idade: ${idade} - Maior De Idade!`
+        console.log(msgMaiorIdade)
+    }else{
+        perEnsinoMedio = false
+        perFaculdade = false
+        const msgMenorIdade = `Idade: ${idade} - Menor De Idade!`
+        console.log(msgMenorIdade)
+    }
+    if(perEnsinoMedio === 'sim'){
+        const msgEnsinoMedioSim = `Terminou o ensino médio!`
+        console.log(msgEnsinoMedioSim)
+    }else{
+        perFaculdade = false
+        const msgEnsinoMedioNao = `Não terminou o ensino médio!`
+        console.log(msgEnsinoMedioNao)
+    }
+    if(perFaculdade === 'sim'){
+        const msgFaculdadeSim = `Sim, está cursando faculdade!`
+        console.log(msgFaculdadeSim)
+    }else{
+        const msgFaculdadenNao = `Não, não está cursando faculdade!`
+        console.log(msgFaculdadenNao)
+    }
 }
+tresValores()
